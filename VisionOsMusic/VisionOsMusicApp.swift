ed0af6f3834c0379406c23b2310cd1ea92d97458
Dario Mintzer <dmintzer75@hotmail.com>
2023-12-21 12:20:37 -0300

    Initial Commit

---
+++ VisionOsMusic/VisionOsMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOsMusicApp.swift
+//  VisionOsMusic
+//
+//  Created by WKS-MAC01 on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionOsMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
